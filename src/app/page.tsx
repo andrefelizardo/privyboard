48cf676efa7d32497738f97fe28af8d01a61b087
andrefelizardo <andreluis.felizardo@outlook.com>
2025-03-19 15:53:16 -0300

    add dark mode support and enhance homepage layout with login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>PrivyBoard</h1>
+        <div className="flex flex-col gap-4 items-center">
+          <h1 className="text-3xl">PrivyBoard</h1>
+          <Button variant="outline" className="text-1xl">
+            Login
+          </Button>
+        </div>
       </main>
     </div>
   );
