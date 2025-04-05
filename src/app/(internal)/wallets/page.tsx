aabf131f1bdc1a2d7bee714362ce9e597586991e
andrefelizardo <andreluis.felizardo@outlook.com>
2025-04-05 13:41:21 -0300

    feat: add dashboard and layout components for improved user interface

---
+++ src/app/(internal)/wallets/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">Wallets</h1>
+    </div>
+  );
+}
