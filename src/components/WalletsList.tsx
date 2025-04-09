df424048ef3304740d708d2f4fdf758ceea3bb92
andrefelizardo <andreluis.felizardo@outlook.com>
2025-04-09 14:42:07 -0300

    WIP: integrate Sheet component for wallet management in WalletsList

--- src/components/WalletsList.tsx
+++ src/components/WalletsList.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-// import { useWalletStore } from "@/lib/store/useWalletStore";
 import { capitalize, formatWalletAddress } from "@/lib/string";
 import ChainLogo from "@/components/ChainLogo";
 import { usePrivy } from "@privy-io/react-auth";
@@ -8,6 +7,16 @@ import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/constants/queryKeys";
 import { fetchUserWallets } from "@/lib/services/wallets";
 import { Tables } from "../../database.types";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
+import { MoreVertical } from "lucide-react";
 
 export default function WalletsList() {
   const { user } = usePrivy();
@@ -30,26 +39,45 @@ export default function WalletsList() {
   return (
     <div className="w-full md:w-4/5 mx-auto px-4 py-12">
       <section className="flex flex-col gap-4">
-        {data.map((wallet: Tables<"user_wallet_networks">) => (
-          <div
-            className="flex items-center justify-between min-h-[73px] px-6 py-4 bg-transparent border rounded-lg"
-            key={`${wallet.wallet_address}-${wallet.network}`}
-          >
-            <div className="flex items-center gap-2">
-              <span className="flex items-center justify-center rounded-full bg-white p-2">
-                <ChainLogo chain={wallet.network} />
-              </span>
-              <span>
-                Wallet {capitalize(wallet.network || "")}{" "}
-                {formatWalletAddress(wallet.wallet_address || "")}
-              </span>
-            </div>
+        {data.map((wallet: Tables<"user_wallet_networks">) => {
+          const walletName = `Wallet ${capitalize(wallet.network || "")} ${formatWalletAddress(wallet.wallet_address || "")}`;
 
-            <div className="flex flex-col items-end">
-              <span className="text-2xl">$ {wallet.balance?.toFixed(4)}</span>
+          return (
+            <div
+              className="flex items-center justify-between min-h-[73px] px-6 py-4 bg-transparent border rounded-lg"
+              key={`${wallet.wallet_address}-${wallet.network}`}
+            >
+              <div className="flex items-center gap-2">
+                <span className="flex items-center justify-center rounded-full bg-white p-2">
+                  <ChainLogo chain={wallet.network} />
+                </span>
+                <span>{walletName}</span>
+              </div>
+
+              <div className="flex">
+                <span className="text-2xl">$ {wallet.balance?.toFixed(4)}</span>
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button variant="ghost">
+                      <MoreVertical className="h-4 w-4" />
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent className="p-4">
+                    <SheetHeader>
+                      <SheetTitle className="text-2xl">
+                        Manage Wallet
+                      </SheetTitle>
+                      <SheetDescription className="text-1xl">
+                        {walletName}
+                      </SheetDescription>
+                    </SheetHeader>
+                    <div></div>
+                  </SheetContent>
+                </Sheet>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </section>
     </div>
   );
